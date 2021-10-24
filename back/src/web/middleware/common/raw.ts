12a2b2e200d2257bd113ad33ec75e99734d85471
Jonathan Guichard <jona.guich69@gmail.com>
2021-10-25 00:41:53 +0200

    Back -- Increase max payload data to 1 Gb

--- back/src/web/middleware/common/raw.ts
+++ back/src/web/middleware/common/raw.ts
@@ -1,20 +1,19 @@
-import * as  bodyParser from "body-parser";
-import * as cookieParser from 'cookie-parser';
+import * as cookieParser from "cookie-parser";
 import * as cors from "cors";
 import * as compress from "compression";
 import * as methodOverride from "method-override";
+import * as express from "express";
 
 export const middlewares: any[] = [];
 
-
 middlewares.push(
+	express.json({ limit: "1000mb" }),
+	express.urlencoded({
+		limit: "1000mb",
+		extended: true,
+	}),
 	cors(),
 	cookieParser(),
 	compress({}),
-	methodOverride(),
-	bodyParser.json(),
-	bodyParser.urlencoded({
-		extended: true
-	}),
-)
-
+	methodOverride()
+);
